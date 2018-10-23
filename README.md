40bb2b8dc99a444501614dea63b41f88da8d829c
Steve Hamrick <shamrickus@gmail.com>
2018-10-22 18:17:12 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ComputerVision
+
+## Install
+* Visual Studio 2015+ with CMake support
+
+## Compile
+* Open CMakeLists in Visual Studio, it should do the rest
+** Note: make sure to select x64-debug/relase as your build target
---
+++ final.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include "opencv2/opencv.hpp"
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		std::cout << " Usage: display_image ImageToLoadAndDisplay" << std::endl;
+		return -1;
+	}
+	Mat image;
+	image = imread(argv[1], IMREAD_COLOR); // Read the file
+	if (image.empty()) // Check for invalid input
+	{
+		std::cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+	namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+	imshow("Display window", image); // Show our image inside it.
+	waitKey(0); // Wait for a keystroke in the window
+	return 0;
+}
